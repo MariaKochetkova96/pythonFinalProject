a8a10b9f0f9410b6f4f39e64e0ce0c1048f13386
MariaKochetkova96 <khomandyakm.576@gmail.com>
2023-05-16 21:04:14 +0200

    add new DB

--- final_project.py
+++ final_project.py
@@ -1,5 +1,10 @@
 import pandas as pd
-df = pd.read_csv("Monthly_Transportation_Statistics.csv")
-print(df)
+df = pd.read_csv("Highest Holywood Grossing Movies.csv")
+del df['Index']
 
 
+print(df.columns)
+print(df.head(10))
+
+# my_movies = df.to_dict("records")
+# print(my_movies[:10])
