1a30cb62524949ef4a695652a5e912bb8437ed90
MariaKochetkova96 <khomandyakm.576@gmail.com>
2023-05-17 13:57:11 +0200

    The program filters 10 movies of the genre the user wants and saves the movies to csv.

--- final_project.py
+++ final_project.py
@@ -1,10 +1,20 @@
 import pandas as pd
+
 df = pd.read_csv("Highest Holywood Grossing Movies.csv")
 del df['Index']
+desired_genres = input("Hi! What movie did you want to watch today? Enter the genre: ")
+filtered_movies = df[df.Genre.str.contains(desired_genres)]
+random_movies = filtered_movies.sample(n=10)
+print(random_movies[['Title', 'Genre', 'Movie Info']])
+
+random_movies.to_csv('Random_movies.csv', index=False)
 
 
-print(df.columns)
-print(df.head(10))
 
+
+# print(df.columns)
+# print(df.Genre)
+# print(df.head(10))
+# print(df.tail(10))
 # my_movies = df.to_dict("records")
 # print(my_movies[:10])
