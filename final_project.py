3e9fa5c073777d2fb3c9c21815055232958f3da2
MariaKochetkova96 <khomandyakm.576@gmail.com>
2023-05-21 22:03:24 +0200

    Added a condition when the user does not know the genre.

--- final_project.py
+++ final_project.py
@@ -1,13 +1,30 @@
 import pandas as pd
+import requests
 
 df = pd.read_csv("Highest Holywood Grossing Movies.csv")
 del df['Index']
 desired_genres = input("Hi! What movie did you want to watch today? Enter the genre: ")
-filtered_movies = df[df.Genre.str.contains(desired_genres)]
-random_movies = filtered_movies.sample(n=10)
-print(random_movies[['Title', 'Genre', 'Movie Info']])
 
-random_movies.to_csv('Random_movies.csv', index=False)
+filtered_movies = df[df.Genre.str.contains(desired_genres)]
+if desired_genres in filtered_movies:     # When user know genre
+    random_movies = filtered_movies.sample(n=10)
+    print(random_movies[['Title', 'Genre', 'Movie Info']])
+
+else: # Ask the user to enter the name of a movie they like
+    movie_title = input("Enter the name of a movie you like: ")
+    filtered_movies = df[df['Title'].str.contains(movie_title, case=False)]
+
+    if filtered_movies.empty:
+        print("No movies found for the entered title.")
+    else:
+        # Display the matched movies
+        url = f"https://www.google.com/search?q={movie_name}"
+        response = requests.get(url)
+        print("Movies found:")
+        print(filtered_movies[['Title', 'Genre', 'Movie Info']])
+
+
+# random_movies.to_csv('Random_movies.csv', index=False)
 
 
 
