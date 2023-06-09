06c9cc84ddd4079d108fee8f41305cb4b76d1c8f
MariaKochetkova96 <khomandyakm.576@gmail.com>
2023-06-09 17:09:08 +0200

    corrected function

--- final_project.py
+++ final_project.py
@@ -5,21 +5,18 @@ df = pd.read_csv("C:\\Users\\Mariia\\Desktop\\Python2\\pythonProject\\Highest Ho
 df.drop('Index', axis=1, inplace=True)
 
 # Function to search for movies by genre in DF
-def find_movies_by_genre():
-    user_input = input("Enter the genre of movies you are looking for: ").strip()
-    filtered_movies = df[df.Genre.str.contains(user_input, case=False)]
+def find_movies_by_genre(genre_name):
+    filtered_movies = df[df.Genre.str.contains(genre_name, case=False)]
 
     if not filtered_movies.empty:
         random_movies = filtered_movies.sample(n=5)
         print(random_movies[['Title', 'Genre', 'Movie Info']])
-        # print(filtered_movies[['Title', 'Genre', 'Movie Info']])
     else:
         print("No movies found in the entered genre.")
 
 
 # Function to search for movie information
-def find_movie_info():
-    movie_title = input("Enter the title of the movie you want to find information about: ").strip()
+def find_movie_info(movie_title):
     filtered_movies = df[df['Title'].str.contains(movie_title, case=False)]
 
     if filtered_movies.empty:
@@ -28,8 +25,7 @@ def find_movie_info():
         print(filtered_movies[['Title', 'Genre', 'Movie Info']])
 
 # Function to search for similar movies
-def find_similar_movies():
-    movie_title = input("Enter the title of the movie to find similar movies: ").strip()
+def find_similar_movies(movie_title):
     filtered_movies = df[df['Title'].str.contains(movie_title, case=False)]
 
     if filtered_movies.empty:
@@ -42,8 +38,7 @@ def find_similar_movies():
 
 
 # Function to search  movies
-def find_movie_genre():
-    movie_title = input("Enter the title of the movie you like: ").strip()
+def find_movie_genre(movie_title):
     filtered_movies = df[df['Title'].str.contains(movie_title, case=False)]
 
     if filtered_movies.empty:
@@ -68,46 +63,20 @@ if __name__ == '__main__':
         choice = input("Enter your choice (1-5): ")
 
         if choice == "1":
-            find_movies_by_genre()
+            user_input = input("Enter the genre of movies you are looking for: ").strip()
+            find_movies_by_genre(user_input)
         elif choice == "2":
-            find_movie_info()
+            user_input = input("Enter the title of the movie you want to find information about: ").strip()
+            find_movie_info(user_input)
         elif choice == "3":
-            find_similar_movies()
+            user_input = input("Enter the title of the movie to find similar movies: ").strip()
+            find_similar_movies(user_input)
         elif choice == "4":
-            find_movie_genre()
+            user_input = input("Enter the title of the movie you like: ").strip()
+            find_movie_genre(user_input)
         elif choice == "5":
             break
         else:
             print("Invalid choice. Please try again.\n")
 
-# desired_genres = input("Hi! What movie did you want to watch today? Enter the genre: ")
-#
-# filtered_movies = df[df.Genre.str.contains(desired_genres)]
-# if not filtered_movies.empty:    # When user know genre
-#     random_movies = filtered_movies.sample(n=10)
-#     print(random_movies[['Title', 'Genre', 'Movie Info']])
-#
-# else:   # df.empty  Ask the user to enter the name of a movie they like
-#     movie_title = input("Enter the name of a movie you like: ").strip()
-#     filtered_movies = df[df['Title'].str.contains(movie_title, case=False)]
-#
-#     if filtered_movies.empty:
-#         print("No movies found for the entered title.")
-#     else:
-#         # Display the matched movies
-#         url = f"https://www.google.com/search?q={movie_title}"
-#         response = requests.get(url)
-#         print("Movies found:")
-#         print(filtered_movies[['Title', 'Genre', 'Movie Info']])
-
-
-
-
-
 # random_movies.to_csv('Random_movies.csv', index=False)
-
-
-# print(df.head(10))
-# print(df.tail(10))
-# my_movies = df.to_dict("records")
-# print(my_movies[:10])
